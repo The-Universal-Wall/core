d17e57bfa52e0df806876fb45da843201363fbf9
qypol342 <qypol342@gmail.com>
2024-08-01 13:41:21 +0200

    allow cookie to be set

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Substitute environment variables in the NGINX template
-envsubst '$PROXY_DOMAIN' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$PROXY_DOMAIN $COOKIE' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # Start NGINX
 exec "$@"
--- readme.md
+++ readme.md
@@ -3,6 +3,6 @@ docker build -t tuw .
 
 docker run -p 80:80 tuw
 
-docker run -p 80:80 -e PROXY_DOMAIN=www.lemonde.fr tuw
+docker run -p 80:80 -e PROXY_DOMAIN=www.XXXX.fr -e COOKIE='COOKIE' tuw
 
 
